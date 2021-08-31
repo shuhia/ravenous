e604f96f6582e8ba241974bc4f5a7bd4e3ece055
Alex On <shuhia.on@gmail.com>
2021-09-01 00:00:40 +0200

    add component SearchBar

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./SearchBar.css";
+import useBusinessSearch from "./useBusinessSearch";
+
+function SearchBar() {
+  const sortByOptions = {
+    "Best Match": "best_match",
+    "Highest Rated": "highest_rated",
+    "Most Reviewed": "most_reviewed",
+  };
+  return <div></div>;
+}
+
+export default SearchBar;
