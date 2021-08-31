d137acb730bdf7759ea1875d8978dc363aff71d1
Alex On <shuhia.on@gmail.com>
2021-08-31 23:28:37 +0200

    add businessList component

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Business from "../Business/Business";
+import "./BusinessList.css";
+
+function BusinessList() {
+  return (
+    <div className="BusinessList">
+      <Business></Business>
+      <Business></Business>
+      <Business></Business>
+      <Business></Business>
+      <Business></Business>
+    </div>
+  );
+}
+
+export default BusinessList;
